c98e92b0a53861636d2c0596c5a6c6f74561e165
GriffenScheel <42558648+GriffenScheel@users.noreply.github.com>
2018-09-21 20:32:47 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace URLEncoder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool go = true;
+
+            while (go)
+            {
+                Console.Write("Enter the name of the project: ");
+                string project = Console.ReadLine();
+                Console.Write("Enter the activity name: ");
+                string activity = Console.ReadLine();
+
+                string url = ("https://companyserver.com/content/" + project + "/files/" + activity + "/" + activity + "Report.pdf");
+                url = Uri.EscapeUriString(url);
+                Console.Write(url);
+                Console.Write("\nWould you like to make anothert document? (y/n): ");
+                go = Console.ReadLine().ToLower().Equals("y");
+              
+
+            }
+        }
+      
+        
+            }
+        }
+    
